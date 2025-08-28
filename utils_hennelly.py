f00cffbaf6ff2ca21476e344c7b541208ae17b92
Kevin Hennelly <ATCKH22@gmail.com>
2025-08-27 19:46:58 -1000

    updates to code project

--- utils_hennelly.py
+++ utils_hennelly.py
@@ -1,6 +1,6 @@
 
 """
-File: utils_case.py
+File: utils_hennelly.py
 
 Purpose: Reusable header/tagline module for analytics projects.
 
@@ -11,11 +11,8 @@ A short, first-week module to demonstrate key skills:
 - expose a function named get_tagline() that can be imported into other modules
 - run this file as a script via main() using the if __name__ == '__main__' pattern
 
-Author: Denise Case
+Author: Kevin Hennelly
 
-TODO: Replace 'lastname' in the filename with your own last (or unique) name.
-TODO: Update the file name and author in this opening docstring.
-TODO: Remove each TODO line AFTER you successfully complete the item.
 """
 
 #####################################
@@ -59,23 +56,21 @@ logger.info("Logger loaded.")
 # ----------------------------------
 is_accepting_clients: bool = True
 offers_remote_workshops: bool = True
-
-# TODO: Declare and initialize a new boolean variable (e.g. `is_hiring` and set it to False)
+is_hiring: bool = False  # Example of a new boolean variable
 
 # ----------------------------------
 # Define Integer variables
 # ----------------------------------
 current_year: int = 2025
 year_started: int = 2020
-
-# TODO: Declare and initialize a new integer variable (e.g. `number_of_employees` and set it to 25)
+number_of_employees: int = 25  # Example of a new integer variable
 
 # ----------------------------------
 # Define String variables
 # ----------------------------------
-author: str = "Denise Case"  # TODO: change to your name
-organization: str = "Stellar Analytics"  # TODO: change the org name
-motto: str = "Clear. Useful. On time."  # TODO: change the motto
+author: str = "Kevin Hennelly"  
+organization: str = "Pithy Python" 
+motto: str = "Squeezing Data Out of Your Numbers"
 
 # TODO: Declare and initialize a new string variable (e.g. `location` and set it to your city and state)
 
